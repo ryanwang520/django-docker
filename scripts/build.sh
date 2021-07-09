52fc60d6e2c62d2aec1247e76e13bb4f67011e4f
Ryan Wang <hwwangwang@gmail.com>
2021-07-09 11:29:50 +0800

    use poetry to lock dependency

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,15 @@
-!#/bin/sh 
+#!/bin/bash
 set -euxo pipefail
-GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) 
-GIT_COMMIT=$(git rev-parse --short HEAD) 
-IMAGE_NAME="moonshadow/django-docker" 
+
+poetry export -o requirements.txt
+
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+GIT_COMMIT=$(git rev-parse --short HEAD)
+IMAGE_NAME="moonshadow/django-docker"
 docker build -t "$IMAGE_NAME:$GIT_BRANCH" \
              -t "$IMAGE_NAME:$GIT_COMMIT" .
 docker push "$IMAGE_NAME:$GIT_BRANCH"
 docker push "$IMAGE_NAME:$GIT_COMMIT"
 
+
+python scripts/smoke_test.py
--- scripts/smoke_test.py
+++ scripts/smoke_test.py
@@ -3,7 +3,7 @@ import time
 from subprocess import check_call
 from urllib.request import urlopen
 check_call(
-    "docker run --rm --name smoke  -p 8000:8000 -d moonshadow/django-docker:master".split()
+     "docker run --rm --name smoke  -p 8000:8000 -d moonshadow/django-docker:master".split()
 )
 # Wait for the server to start. A better implementation would
 # poll in a loop:
@@ -11,12 +11,12 @@ def check():
     count = 0
     while True:
         time.sleep(0.5)
-        # Check if the server started (it'll throw an exception if not): 
+        # Check if the server started (it'll throw an exception if not):
         try:
-            urlopen("http://localhost:8000/debug").read() 
+            urlopen("http://localhost:8000/debug").read()
         except Exception:
             if count < 5:
-                count +=1 
+                count +=1
                 continue
             else:
                 raise
