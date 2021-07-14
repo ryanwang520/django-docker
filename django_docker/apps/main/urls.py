573eda45a959e2b58fd785f5cb54a780d6931e8b
Ryan Wang <hwwangwang@gmail.com>
2021-07-14 15:09:40 +0800

    lint

--- django_docker/apps/main/urls.py
+++ django_docker/apps/main/urls.py
@@ -1,8 +1,7 @@
+from django.urls import path
 
-from django.contrib import admin
-from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('go/', views.go),
+    path("go/", views.go),
 ]
--- django_docker/apps/main/views.py
+++ django_docker/apps/main/views.py
@@ -1,8 +1,10 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+
 from .models import Person
 
 # Create your views here.
+
+
 def go(request):
-	persons = Person.objects.all()
-	return HttpResponse(f"go {len(persons)} persons")
+    persons = Person.objects.all()
+    return HttpResponse(f"go {len(persons)} persons")
